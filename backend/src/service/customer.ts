98cc8962b8c4661f9dbbd0b57a9bc6ae484ab407
Lucas <lucaschmain@gmail.com>
2024-03-13 17:42:32 -0300

    feat: adding post customer service

--- backend/src/service/customer.ts
+++ backend/src/service/customer.ts
@@ -3,3 +3,33 @@ export async function getCustomerService() {
 
   return { customerList }
 }
+
+interface IPostCustomerData {
+  fullName: string
+  email: string
+  cpf: string
+  phone: string
+  status: string
+}
+
+export async function postCustomerService({
+  fullName,
+  email,
+  cpf,
+  phone,
+  status,
+}: IPostCustomerData) {
+  try {
+    await prisma.create({
+      data: {
+        fullName,
+        email,
+        cpf,
+        phone,
+        status,
+      },
+    })
+  } catch (_error) {
+    throw new Error('Failed to create a new customer')
+  }
+}
