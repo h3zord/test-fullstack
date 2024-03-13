2307f8e36d8d1798fae7854f1ac4dafde4631fc4
Lucas <lucaschmain@gmail.com>
2024-03-13 19:44:24 -0300

    feat: adding error handler middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { customerRouter } from './routes/customer'
 import { LoginRouter } from './routes/login'
+import { errorHandler } from './middlewares/errorHandler'
 
 export const app = express()
 
@@ -8,3 +9,5 @@ app.use(express.json())
 
 app.use('/login', LoginRouter)
 app.use('/customer', customerRouter)
+
+app.use(errorHandler)
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,8 @@
 import 'dotenv/config'
 import { app } from './app'
 
-const port = process.env.PORT || '3000'
+const PORT = process.env.PORT || '3000'
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`)
 })
