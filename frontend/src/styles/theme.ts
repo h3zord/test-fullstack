d98b0a9ae5874aa0dbdf4ce1c5f97958f91fc400
Lucas <lucaschmain@gmail.com>
2024-03-14 19:58:26 -0300

    styles: changing default theme

--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -1,6 +1,6 @@
 export const defaultTheme = {
   fonts: {
-    default: 'Nunito, sans-serif',
+    default: 'Roboto, sans-serif',
   },
 
   space: {
@@ -26,9 +26,10 @@ export const defaultTheme = {
   },
 
   fontWeights: {
+    thin: '300',
     regular: '400',
-    medium: '500',
     bold: '700',
+    'extra-bold': '900',
   },
 
   lineHeights: {
@@ -43,29 +44,23 @@ export const defaultTheme = {
     black: '#000000',
 
     green: {
-      100: '#50B2C0',
-      200: '#255D6A',
-      300: '#0A313C',
-    },
-
-    purple: {
-      100: '#8381D9',
-      200: '#2A2879',
+      500: '"#006600"',
     },
 
     gray: {
-      100: '#F8F9FC',
-      200: '#E6E8F2',
-      300: '#D1D6E4',
-      400: '#8D95AF',
-      500: '#303F73',
-      600: '#252D4A',
-      700: '#181C2A',
-      800: '#0E1116',
+      300: '#dfdfdf',
+      900: '#151515',
     },
 
-    'gradient-vertical': `linear-gradient(180deg, #7FD1CC 0%, #9694F5 100%)`,
-    'gradient-horizontal': `linear-gradient(90deg, #7FD1CC 0%, #9694F5 100%)`,
+    yellow: {
+      400: '#f0c02c',
+      700: '#dfa700',
+    },
+
+    red: {
+      400: '#FF0000',
+      500: '#CC0000',
+    },
   },
 
   radii: {
