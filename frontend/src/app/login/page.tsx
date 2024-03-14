3f8a2a832a7f579519fe2521307ac9b037a44928
Lucas <lucaschmain@gmail.com>
2024-03-14 12:43:10 -0300

    feat: adding login page

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,5 +1,70 @@
-export default async function Login() {
-  await new Promise((resolve) => setTimeout(resolve, 2000))
+'use client'
 
-  return <h1>Login</h1>
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+
+export default function Login() {
+  const loginDataSchema = z
+    .object({
+      email: z
+        .string({
+          required_error: 'Digite seu email!',
+        })
+        .email({
+          message: 'Digite seu email corretamente! ',
+        })
+        .toLowerCase(),
+      password: z
+        .string({
+          required_error: 'Digite sua senha!',
+        })
+        .min(5, {
+          message: 'A senha deve possuir no mínimo 5 dígitos!',
+        }),
+    })
+    .required()
+
+  type TLoginDataSchema = z.infer<typeof loginDataSchema>
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm<TLoginDataSchema>({
+    resolver: zodResolver(loginDataSchema),
+  })
+
+  function handleSubmitLogin(loginData: TLoginDataSchema) {
+    console.log(loginData)
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit(handleSubmitLogin)}
+      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+    >
+      <input
+        placeholder="Digite seu email!"
+        type="text"
+        {...register('email')}
+      />
+      {errors.email && <p>{errors.email.message}</p>}
+      <input
+        placeholder="Digite sua senha!"
+        type="password"
+        {...register('password')}
+      />
+      {errors.password && <p>{errors.password.message}</p>}
+
+      <div>
+        <button type="submit" disabled={isSubmitting}>
+          Login
+        </button>
+        <button type="submit">Entrar como visitante</button>
+      </div>
+
+      <button>Github</button>
+    </form>
+  )
 }
