4abb339858f428e4578adbc1baad2e7ab606829c
Lucas <lucaschmain@gmail.com>
2024-03-18 14:47:51 -0300

    test: writing unitary tests on backend

---
+++ backend/tests/controller/customer.test.ts
@@ -0,0 +1,110 @@
+import { Request, Response } from 'express'
+import { TCreateAndUpdateCustomerSchema } from '../../src/validate-schema/customer'
+import {
+  createCustomerService,
+  readCustomerService,
+  updateCustomerService,
+} from '../../src/service/customer'
+import {
+  createCustomerController,
+  readCustomerController,
+  updateCustomerController,
+} from '../../src/controller/customer'
+
+jest.mock('../../src/service/customer', () => ({
+  readCustomerService: jest.fn(),
+  createCustomerService: jest.fn(),
+  updateCustomerService: jest.fn(),
+}))
+
+describe('Testing functions on CustomerController layer', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should return list of customers from the service', async () => {
+    const mockCustomerList: TCreateAndUpdateCustomerSchema[] = [
+      {
+        fullName: 'Lucas Test',
+        email: 'test@test.com',
+        cpf: '00000000000',
+        phone: '00000000000',
+        status: 'Ativo',
+      },
+      {
+        fullName: 'Admin',
+        email: 'admin@test.com',
+        cpf: '99999999999',
+        phone: '99999999999',
+        status: 'Desativado',
+      },
+    ]
+
+    ;(readCustomerService as jest.Mock).mockResolvedValueOnce({
+      customerList: mockCustomerList,
+    })
+
+    const req = {} as Request
+    const jsonMock = jest.fn()
+    const statusMock = jest.fn().mockReturnValue({ json: jsonMock })
+    const res = { status: statusMock } as unknown as Response
+
+    await readCustomerController(req, res)
+
+    expect(statusMock).toHaveBeenCalledWith(200)
+    expect(jsonMock).toHaveBeenCalledWith(mockCustomerList)
+  })
+
+  it('should return success message for creating customer', async () => {
+    const customerData: TCreateAndUpdateCustomerSchema = {
+      fullName: 'Admin',
+      email: 'admin@example.com',
+      cpf: '11111111111',
+      phone: '11111111111',
+      status: 'Ativo',
+    }
+
+    ;(createCustomerService as jest.Mock).mockResolvedValueOnce(undefined)
+
+    const req = { body: customerData } as Request
+    const jsonMock = jest.fn()
+    const statusMock = jest.fn().mockReturnValue({ json: jsonMock })
+    const res = { status: statusMock } as unknown as Response
+
+    await createCustomerController(req, res)
+
+    expect(statusMock).toHaveBeenCalledWith(201)
+    expect(jsonMock).toHaveBeenCalledWith({
+      success: 'Cliente criado com sucesso!',
+    })
+  })
+
+  it('should return success message for updating customer', async () => {
+    const customerId = '1'
+    const customerData: TCreateAndUpdateCustomerSchema = {
+      fullName: 'Admin Test',
+      email: 'test@example.com',
+      cpf: '44444444444',
+      phone: '22222222222',
+      status: 'Aguardando ativação',
+    }
+
+    ;(updateCustomerService as jest.Mock).mockResolvedValueOnce(undefined)
+
+    const req = {
+      params: { id: customerId },
+      body: customerData,
+    } as unknown as Request
+
+    const jsonMock = jest.fn()
+    const statusMock = jest.fn().mockReturnValue({ json: jsonMock })
+    const res = { status: statusMock } as unknown as Response
+
+    await updateCustomerController(req, res)
+
+    expect(statusMock).toHaveBeenCalledWith(200)
+    expect(jsonMock).toHaveBeenCalledWith({
+      success: 'Cliente atualizado com sucesso!',
+    })
+  })
+})
---
+++ backend/tests/service/customer.test.ts
@@ -0,0 +1,40 @@
+import { readCustomerService } from '../../src/service/customer'
+import { prisma } from '../../src/lib/prisma'
+import { TCreateAndUpdateCustomerSchema } from '../../src/validate-schema/customer'
+
+jest.mock('@prisma/client', () => ({
+  PrismaClient: jest.fn().mockImplementation(() => ({
+    customer: {
+      findMany: jest.fn(),
+    },
+  })),
+}))
+
+describe('readCustomerService', () => {
+  it('should return customer list', async () => {
+    const mockCustomerList: TCreateAndUpdateCustomerSchema[] = [
+      {
+        fullName: 'Lucas Test',
+        email: 'test@test.com',
+        cpf: '00000000000',
+        phone: '00000000000',
+        status: 'Ativo',
+      },
+      {
+        fullName: 'Admin',
+        email: 'admin@test.com',
+        cpf: '99999999999',
+        phone: '99999999999',
+        status: 'Desativado',
+      },
+    ]
+
+    ;(prisma.customer.findMany as jest.Mock).mockResolvedValueOnce(
+      mockCustomerList,
+    )
+
+    const result = await readCustomerService()
+
+    expect(result).toEqual({ customerList: mockCustomerList })
+  })
+})
